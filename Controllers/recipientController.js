495451e1a47cd6183cedb2bd42346523d0a03333
Emaan20 <ema59983@gmail.com>
2024-08-29 20:36:45 +0500

    new file:   Controllers/recipientController.js 	modified:   Models/recepientsModel.js 	modified:   Routes/reciepientRoutes.js 	modified:   server.js

---
+++ Controllers/recipientController.js
@@ -0,0 +1,35 @@
+const Recipient = require('../Models/recepientsModel');
+
+const addRecipient = async ({ patientName, bloodgroup, city, phoneNumber, age, lastDonated }) => {
+    const newRecipient = new Recipient({
+        patientName,
+        bloodgroup,
+        city,
+        phoneNumber,
+        age,
+        lastDonated
+    });
+
+    await newRecipient.save();
+    return newRecipient;
+}
+
+const findDuplicate = async (phoneNumber) => {
+    return await Recipient.find({ phoneNumber });
+}
+
+const getRecipient = async (bloodgroup, city) => {
+    const query = {};
+    if (bloodgroup) query.bloodgroup = bloodgroup;
+    if (city) query.city = city;
+
+    return await Recipient.find(query)
+        .sort({ patientName: 1 })
+        .select({ patientName: 1, bloodgroup: 1, city: 1, phoneNumber: 1, age: 1, lastDonated: 1 });
+}
+
+module.exports = {
+    addRecipient,
+    findDuplicate,
+    getRecipient
+};
